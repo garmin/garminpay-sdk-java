de72729426df85082a5427ecd54f7ba4d55ce433
Sebastian.Morton-Zeller <sebastian.morton-zeller@garmin.com>
2025-10-09 17:39:08 +0000

    Update SDKVersion.java

--- src/main/java/com/garmin/garminpay/model/SDKVersion.java
+++ src/main/java/com/garmin/garminpay/model/SDKVersion.java
@@ -10,5 +10,5 @@ public final class SDKVersion {
     private SDKVersion() {
     }
 
-    public static final String VERSION = "0.9.0-SNAPSHOT";
+    public static final String VERSION = "1.0.3";
 }
